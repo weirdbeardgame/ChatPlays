42978a719f714d49d67638f8b22fea046890541d
t o <finalfantasytimothy@gmail.com>
2022-02-18 19:10:11 -0700

    Added quick ToDo note

--- Header/PineClient.h
+++ Header/PineClient.h
@@ -20,6 +20,8 @@ private:
 	// This represents the active emulator. Use polymorphisim. There's class PCSX2, and class RPCS3 etc.
 	Shared *emulator;
 
+	// ToDo Add Tas.
+
 	// This is a map of potential commands and functions that execute them or. You can use a switch case in the run function to parse the command.
 	std::map<std::string, void*> commands;
 public:
